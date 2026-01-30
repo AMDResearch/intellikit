30e781f37e34b3428e1ffd14c5a03d3a76d6f6f3
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-01-30 15:57:24 -0500

    feat: update license headers with SPDX identifier

--- uprof_mcp/examples/uprof_profiler.py
+++ uprof_mcp/examples/uprof_profiler.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025-2026 Advanced Micro Devices, Inc. All rights reserved.
 
 import argparse
 import asyncio
